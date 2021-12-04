7204a6e578194f7b4694ac9b210cbeebedca41e7
Darren <darren@driveyello.com>
2021-12-04 13:18:23 +1100

    Unused dependencies

--- src/__tests__/DropdownListItem.test.tsx
+++ src/__tests__/DropdownListItem.test.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from '../App';
-import CountryInput from '../components/CountryInput';
 import DropdownListItem from '../components/DropdownListItem';
 
 const dummyLiProps = {
